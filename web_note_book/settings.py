e0cecbcae830b8ed01c8952910ebf938eff30c35
rahul kathait <rahul.kathait011@gmail.com>
2021-09-08 03:43:40 -0700

    set SECRET_KEY

--- web_note_book/settings.py
+++ web_note_book/settings.py
@@ -19,12 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9wsve$hq8#m5il!+vmhcau!hpl!n94=*75r0xk%s)!^s$i^ovp'
+# SECRET_KEY = 'django-insecure-9wsve$hq8#m5il!+vmhcau!hpl!n94=*75r0xk%s)!^s$i^ovp'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["web-note-book.herokuapp.com"]
 
 # Application definition
 
